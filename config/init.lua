d28fb2a88fca33e2b0064fc175b9df7cf7bc0782
Samuel Souza <samuelcvr2@gmail.com>
2022-04-06 18:42:54 -0300

    remove missplaced file

--- config/init.lua
+++
@@ -1,9 +0,0 @@
--- Source lua files
-require('user.options')
-require('user.plugins')
-require('user.telescope') -- Proper keybinds for this fella later.
--- require('user.whichkey')
-require('user.keybinds')
-require('user.colorscheme')
-require('user.lsp')
--- require('user.neoscroll') - Disabled because I won't use 0 value for scrolloff option
