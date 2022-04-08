c15238cc3e4d08900d2290ca642b96d230df4848
Samuel Souza <samuelcvr2@gmail.com>
2022-04-08 17:50:05 -0300

    more autocomplete stuff

--- nvim/.config/nvim/lua/user/complete.lua
+++ nvim/.config/nvim/lua/user/complete.lua
@@ -9,19 +9,45 @@ cmp.setup{
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.close(),
         ["<C-y>"] = cmp.config.disabled,
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-        { name = 'buffer', keyword_length = 3 },
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+        { name = "nvim_lsp_document_symbol" },
+        -- { name = "ultisnips" }, -- For ultisnips users.
+        -- { name = "snippy" }, -- For snippy users.
+        -- { name = "vsnip" }, -- For vsnip users.
+        { name = "path" },
+        { name = "buffer", keyword_length = 3 },
     },
     experimental = {
         ghost_text = true,
         native_menu = false,
-    }
+    },
+    snippet = {
+        expand = function (args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
 }
 
--- highlight menu?
+-- native_menu option must be disable for this to work
+cmp.setup.cmdline('/', {
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+        { name = 'path' }
+        }, {
+        { name = 'cmdline' }
+    })
+})
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    require('lspconfig')['gopls'].setup {
+    capabilities = capabilities
+}
