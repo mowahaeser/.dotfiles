a79c14286a793d14eed5c3c1e265d52395499ff7
Samuel Souza <samuelcvr2@gmail.com>
2022-04-19 19:28:05 -0300

    add capabilities

--- nvim/.config/nvim/lua/user/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp-installer.lua
@@ -1,11 +1,81 @@
-local status_ok, lsp_installer pcall(require, "nvim-lsp-installer")
-if not status_ok then
-    return
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local on_attach = function(client, bufnr)
+
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
+
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	-- leaving only what I actually use...
+	buf_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+
+	buf_set_keymap("n", "<C-j>", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+
+	buf_set_keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "<leader>D", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>ca", "<cmd>Telescope lsp_code_actions<CR>", opts)
+
+    if client.resolved_capabilities.document_formatting then
+		vim.cmd([[
+			augroup formatting
+				autocmd! * <buffer>
+				autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
+				autocmd BufWritePre <buffer> lua OrganizeImports(1000)
+			augroup END
+		]])
+	end
+
+	-- Set autocommands conditional on server_capabilities
+	if client.resolved_capabilities.document_highlight then
+		vim.cmd([[
+			augroup lsp_document_highlight
+				autocmd! * <buffer>
+				autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+				autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+			augroup END
+		]])
+	end
 end
 
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-    server:setup(opts)
+local lua_opts = {}
+
+lua_opts["gopls"] = {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		gopls = {
+			gofumpt = true,
+		},
+	},
+	flags = {
+		debounce_text_changes = 150,
+	},
+}
+
+require("nvim-lsp-installer").on_server_ready(function(server)
+    server:setup(lua_opts[server.name] or {})
 end)
 
-
+function OrganizeImports(timeoutms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
+end
--- nvim/.config/nvim/lua/user/lsp-setup.lua
+++ nvim/.config/nvim/lua/user/lsp-setup.lua
@@ -1,27 +1,27 @@
-local status_ok = pcall(require, "lspconfig")
-if not status_ok then
-    return
-end
-
-local map_options = {
-    noremap = true,
-    silent = true,
-}
-
-local on_attach = function(client, bufnr)
-    require "lsp_signature".on_attach({
-            bind = true, -- This is mandatory, otherwise border config won't get registered.
-            handler_opts = {
-            border = "rounded"
-        }
-    }, bufnr)
-end
-
-
-local servers = { "gopls", "tsserver", "pyright", "bashls", "dockerls", "jsonls", "html", "sumneko_lua" }
-
-for _, lsp in pairs(servers) do
-    require("lspconfig")[lsp].setup {
-        on_attach = on_attach,
-    }
-end
+-- local status_ok = pcall(require, "lspconfig")
+-- if not status_ok then
+--     return
+-- end
+-- 
+-- local map_options = {
+--     noremap = true,
+--     silent = true,
+-- }
+-- 
+-- local on_attach = function(client, bufnr)
+--     require "lsp_signature".on_attach({
+--             bind = true, -- This is mandatory, otherwise border config won't get registered.
+--             handler_opts = {
+--             border = "rounded"
+--         }
+--     }, bufnr)
+-- end
+-- 
+-- 
+-- local servers = { "gopls", "tsserver", "pyright", "bashls", "dockerls", "jsonls", "html", "sumneko_lua" }
+-- 
+-- for _, lsp in pairs(servers) do
+--     require("lspconfig")[lsp].setup {
+--         on_attach = on_attach,
+--     }
+-- end
